5e7d1ae9ac2d2943c088799ac902063f1ba38c9a
Eric Bishard <e.bishard@gmail.com>
2019-05-09 12:00:46 -0400

    update title message to "Todo App" in `Home.js`

--- app/view-components/Home.js
+++ app/view-components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-  document.title = `TripXpert Home`;
+  document.title = `Todo App`;
   return (
     <div className="view-home">
       <h1>A simple React project with routing and todos.</h1>
